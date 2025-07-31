29e391253809e5ecbb05a16872be664b9e0a9d5f
Beca-re <pedroso.rebeca@escola.pr.gov.br>
2025-07-31 11:08:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+const perguntas = [
+    {
+    categoria: "[Desenho animado]",
+    pergunta: "[qual o formato da casa do Bob esponja?]",
+    resposta: "[Um abacaxi]"
+    },
+    {
+    categoria: "[Desenho animado]",
+    pergunta: "[Qual animal Alice persegue?]",
+    resposta: "[Um coelho]"
+    },
+    {
+    categoria: "[Desenho animado]",
+    pergunta: "[Qual o nome do camaleão da Rapunzel?]",
+    resposta: "[Pascal]"
+    },
+    {
+    categoria: "[Desenho animado]",
+    pergunta: "[Quantos anões a Branca de Neve tem?]",
+    resposta: "[7 anões]"
+    },
+    {
+    categoria: "[Desenho animado]",
+    pergunta: "[Qual fruta virou uma carruagem para a Cinderela?]",
+    resposta: "[Uma abóbora]"
+    }
+    ];
+    
